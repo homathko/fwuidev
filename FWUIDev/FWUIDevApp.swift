dc64040fdf757aaa2990d8f2eeed3965e01f98c7
elight <eric@homathkotech.com>
2021-05-20 10:33:48 -0700

    Attempt to turn nav bar translucent, failed but kept in comments

--- FWUIDev/FWUIDevApp.swift
+++ FWUIDev/FWUIDevApp.swift
@@ -6,9 +6,21 @@
 //
 
 import SwiftUI
+import UIKit
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+    func applicationDidFinishLaunching (_ application: UIApplication) {
+        /// No effect
+//        UINavigationBar.appearance().setBackgroundImage(UIImage(), for: .default)
+//        UINavigationBar.appearance().shadowImage = UIImage()
+//        UINavigationBar.appearance().backgroundColor = .clear
+//        UINavigationBar.appearance().isTranslucent = true
+    }
+}
 
 @main
 struct FWUIDevApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
     var body: some Scene {
         WindowGroup {
             ContentView()
